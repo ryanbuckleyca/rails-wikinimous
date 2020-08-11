8259c5b9610ee37b24d8e24d47fac72568a1b1d1
Ryan Buckley <ryanbuckley@gmail.com>
2020-08-11 15:54:15 -0400

    created mock wikinimous page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+10.times do
+  new_article = Article.new(title: Faker::Lorem.sentence, content: Faker::Lorem.paragraph)
+  new_article.save
+end
